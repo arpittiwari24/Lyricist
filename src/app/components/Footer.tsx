7973831f27613fba251cd86af4622698b52d5242
arpittiwari24 <100207940+arpittiwari24@users.noreply.github.com>
2023-10-09 20:51:03 +0530

    last commit

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,16 @@
+
+
+export default function Footer() {
+    return (
+        <>
+        <div className="flex justify-center items-center pb-10">
+        <a href="https://www.producthunt.com/posts/lyricist?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-lyricist" target="_blank">
+            <img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=418969&theme=light" alt="Lyricist - Your AI-based Lyrics generator | Product Hunt" />
+        </a>
+    </div>
+    <div className="text-center py-4">
+        <p>Made with love by <a className="underline text-blue-400" href="https://arrpit.netlify.app/" target="blank">Arrpit</a></p>
+    </div>
+    </>
+)
+}
